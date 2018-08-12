9ef2753f4168e7cfca0edb84003ba8ec67ef2ba3
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-08-12 07:51:12 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,8 +1,15 @@
 class CashRegister
   attr_accessor :total
+  @@discount
   # attr_accessor :discount
   
   def initialize()
     @total = 0
+  end
+  
+  def self.cash_register_with_discount(discount)
+    cashRegister = self.new
+    @@discount = discount
+    cashRegister
   end
 end
