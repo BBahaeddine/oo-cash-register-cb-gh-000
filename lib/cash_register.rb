7285d5ce4be153882020fba25e0e29c802780d6a
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-08-12 08:08:06 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -17,7 +17,7 @@ class CashRegister
   end
   
   def apply_discount
-      total = @total - ((@total * @@discount) / 100)
+      @total = @total - ((@total * @@discount) / 100)
       return "After the discount, the total comes to $#{@total}."
   end
 end
