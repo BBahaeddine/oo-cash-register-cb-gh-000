e7cd3b70dd902dc8aac7e803c6a17b275cc20016
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-08-12 08:01:57 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -20,7 +20,7 @@ class CashRegister
     if @@discount == 0 
       return "There is no discount to apply."
     else
-      total = @total - ((@total * @@discount / 100))
+      total = @total - ((@total * @@discount) / 100)
       return "After the discount, the total comes to $#{@total}."
     end
   end
